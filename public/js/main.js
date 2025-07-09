038273b36a6eb5aa28fbe05ba71d2818012dcf55
RCAgnes <rcurbaniagnes@gmail.com>
2025-07-09 00:32:29 -0400

    updates

--- public/js/main.js
+++ public/js/main.js
@@ -5,10 +5,27 @@ new Glide('.glide').mount();
 AOS.init();
 
 // Leaflet Map
-const map = L.map('map').setView([-17.6509, -149.4260], 8);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; OpenStreetMap contributors'
-}).addTo(map);
-L.marker([-17.6509, -149.4260]).addTo(map)
-  .bindPopup('Papeete, Tahiti - French Polynesia')
-  .openPopup();
+// Inicialize o mapa Leaflet apenas se o elemento existir
+if (document.getElementById('map')) {
+  var map = L.map('map').setView([-17.6509, -149.4260], 10);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+  }).addTo(map);
+
+  var marker = L.marker([-17.6509, -149.4260]).addTo(map);
+  marker.bindPopup("<b>Tahiti</b><br>Welcome to Tahiti!").openPopup();
+}
+
+// Glide para a galeria
+if (document.querySelector('.gallery-glide')) {
+  new Glide('.gallery-glide', {
+    type: 'carousel',
+    perView: 1,
+    autoplay: 2500,
+    hoverpause: true,
+    animationDuration: 900,
+    animationTimingFunc: 'linear'
+  }).mount();
+}
